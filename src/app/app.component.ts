c53f5f9f6a9a07a6257f6057b9f739c0617c7eeb
ShahzodaD <daminovashahzoda667@gmail.com>
2024-03-12 09:15:34 +0300

    projectH

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { AboutUsComponent } from "./about-us/about-us.component";
+import { ServicesComponent } from "./services/services.component";
+import { TeamComponent } from "./team/team.component";
+import { HappyComponent } from "./happy/happy.component";
+import { FooterComponent } from "./footer/footer.component";
+import { BlogComponent } from "./blog/blog.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent, AboutUsComponent, ServicesComponent, TeamComponent, HappyComponent, FooterComponent, BlogComponent]
 })
 export class AppComponent {
   title = 'HosPI';
